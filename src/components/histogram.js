8b02d9dddc69472355b89335d8c5b76d317e2c9b
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-04-11 12:00:47 -0600

    histogram rendering with hard coded data

---
+++ src/components/histogram.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Histogram from 'react-chart-histogram';
+
+const HistogramComponent = () => {
+    const labels = ['2016', '2017', '2018'];
+    const data = [324, 45, 672];
+    const options = { fillColor: '#FFFFFF', strokeColor: '#0000FF' };
+    return (
+        <div>
+            <Histogram
+                xLabels={labels}
+                yValues={data}
+                width='400'
+                height='200'
+                options={options}
+            />
+        </div>
+    )
+
+}
+
+export default HistogramComponent
